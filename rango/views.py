fb3418929d83ddaac9d882bdf9792fedabf5266b
“Yingjia <2414112l@student.gla.ac.uk>
2019-01-21 00:15:43 +0000

    c5.5

--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
+
+#def about(request):
+ #   context_dict = {'aboutme': "This tutorial has been put together by Yingjia L."}
+  #  return render(request, 'rango/about.html', context=context_dict)
