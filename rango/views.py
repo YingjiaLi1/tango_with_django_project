e84a8e71c869ffd74b4faa1483dd57217198bcaa
“Yingjia <2414112l@student.gla.ac.uk>
2019-01-21 11:28:13 +0000

    chapter 4

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,6 @@ def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
-#def about(request):
- #   context_dict = {'aboutme': "This tutorial has been put together by Yingjia L."}
-  #  return render(request, 'rango/about.html', context=context_dict)
+def about(request):
+    context_dict = {'aboutme': "This tutorial has been put together by Yingjia L."}
+    return render(request, 'rango/about.html', context=context_dict)
