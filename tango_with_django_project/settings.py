4d6e6af11a45f5c1421579581e9a794f7a6d1731
“Yingjia <2414112l@student.gla.ac.uk>
2019-01-30 10:03:21 +0000

    chapter 11

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration'
 ]
 
 MIDDLEWARE = [
@@ -139,4 +140,10 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
-LOGIN_URL = '/rango/login/'
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+
+LOGIN_URL = '/accounts/login/'
